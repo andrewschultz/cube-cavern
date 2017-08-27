7ef22360e7970c48931ece6a4f12abc774768023
andrewschultz <schultz.andrew@sbcglobal.net>
2017-08-26 21:15:41 -0500

    rube cube generated map

---
+++ t2t.py
@@ -0,0 +1,18 @@
+import re
+
+chg = {}
+
+with open("t2t.txt") as file:
+	for line in file:
+		if re.search("\t", line):
+			x = line.strip().split("\t")
+			chg[x[0]] = x[1]
+
+fout = open("c:\\games\\inform\\triz\\mine\\rube-cube-2.trizbort", "w")
+
+with open("c:\\games\\inform\\triz\\mine\\rube-cube.trizbort") as file:
+	for line in file:
+		line2 = re.sub("name=\"([udnews]00)", (lambda x: "name=\"" + chg[x.group(1)]), line, re.IGNORECASE)
+		fout.write(line2)
+
+fout.close()
