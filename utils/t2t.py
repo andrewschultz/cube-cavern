a1ebf94bd506c244719963c7ccdf46759a6f903d
andrewschultz <schultz.andrew@sbcglobal.net>
2017-09-22 21:18:39 -0500

    name shuffling

--- utils/t2t.py
+++ utils/t2t.py
@@ -12,9 +12,9 @@ def c2(x):
     temp = chg[x]
     return re.sub(" face, ", "/", temp, re.IGNORECASE)
 
-fout = open("c:\\games\\inform\\triz\\mine\\rube-cube-2.trizbort", "w")
+fout = open("c:\\games\\inform\\triz\\mine\\cube-sky-orig.trizbort", "w")
 
-with open("c:\\games\\inform\\triz\\mine\\rube-cube.trizbort") as file:
+with open("c:\\games\\inform\\triz\\mine\\cube-sky-map.trizbort") as file:
     for line in file:
         line2 = re.sub(r"name=\"([udnews][0-2]{2})", (lambda x: "name=\"" + chg[x.group(1)]), line, re.IGNORECASE)
         line2 = re.sub(r"(midText|startText)=\"([udnews][0-2]{2})", (lambda x: x.group(1) + "=\"" + c2(x.group(2))), line2, re.IGNORECASE)
